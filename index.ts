b99bdcc6ac59c6f6f3ce2db8fc99a27eb670e1a0
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-26 21:07:11 +0700

    1.2 Tạo route cơ bản

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Trang Chu");
+app.get("/tours", (req: Request, res: Response) => {
+    res.send("Danh sach tour");
 });
 
 app.listen(port, () => {
