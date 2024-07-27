7883cbc58ee41aa9662ea65a7b02a1451f90a7e0
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-27 23:55:40 +0700

    2.2 Hiển thị thông báo đã thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -19,6 +19,22 @@ var imagesMain = new Swiper(".imagesMain", {
 });
 //@ End Slider Tour Detail
 
+const alertAddCartSuccess = () => {
+    const elementAlert = document.querySelector("[alert-add-cart-success]");
+    if (elementAlert) {
+        elementAlert.classList.remove("alert-hidden");
+
+        setTimeout(() => {
+            elementAlert.classList.add("alert-hidden");
+        }, 3000);
+
+        const closeAlert = elementAlert.querySelector("[close-alert]");
+        closeAlert.addEventListener("click", () => {
+            elementAlert.classList.add("alert-hidden");
+        });
+    }
+};
+
 //@ Carts
 const cart = localStorage.getItem("cart");
 //@@ Nếu chưa có giỏ hàng thì tạo giỏ hàng mới cho người dùng
@@ -53,6 +69,8 @@ if (formAddToCart) {
             }
 
             localStorage.setItem("cart", JSON.stringify(cart));
+
+            alertAddCartSuccess();
         }
     });
 }
