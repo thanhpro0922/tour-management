eb8fb65ebe5e4460ef5f97cd88c4a2a2cef3fb71
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-27 18:43:16 +0700

    2.2 Tạo controller

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -1,16 +1,9 @@
-import { Router, Request, Response } from "express";
-import Tour from "../../models/tour.model";
+import { Router } from "express";
+
+import * as controller from "../../controllers/client/tour.controller";
 
 const router: Router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
-    const tours = await Tour.findAll({
-        raw: true /*Tránh trả ra mấy key linh tinh*/,
-    });
-
-    res.render("client/pages/tours/index.pug", {
-        tours: tours,
-    });
-});
+router.get("/", controller.index);
 
 export const tourRoutes: Router = router;
