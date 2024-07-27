a41f402fbcf989629394517cfdad16943b0c1019
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-27 22:46:49 +0700

    1.2. Lấy data in ra giao diện

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import sequelize from "../../config/database";
 import { QueryTypes } from "sequelize";
+import Tour from "../../models/tour.model";
 
 //@ [GET] /tours/:slugCategory
 export const index = async (req: Request, res: Response) => {
@@ -39,9 +40,23 @@ export const index = async (req: Request, res: Response) => {
 export const detail = async (req: Request, res: Response) => {
     const slugTour = req.params.slugTour;
 
-    console.log(slugTour);
+    const tourDetail = await Tour.findOne({
+        where: {
+            slug: slugTour,
+            deleted: false,
+            status: "active",
+        },
+        raw: true,
+    });
+
+    if (tourDetail["images"]) {
+        tourDetail["images"] = JSON.parse(tourDetail["images"]);
+    }
+    tourDetail["price_special"] =
+        tourDetail["price"] * (1 - tourDetail["discount"] / 100);
 
     res.render("client/pages/tours/detail", {
         pageTitle: "Chi tiet tour",
+        tourDetail,
     });
 };
