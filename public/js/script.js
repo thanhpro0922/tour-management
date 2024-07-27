8ad7318f59882ff3fd64e45e8e4b6030d2ccb587
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-27 23:29:04 +0700

    2.1 Thêm tour vào giỏ hàng (lưu bằng localStorage chứ ko lưu bằng database, còn thích thì lưu vào database cx đc, ở đây là học cách lưu này của bên frontend)

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,43 @@ var imagesMain = new Swiper(".imagesMain", {
         swiper: imagesThumb,
     },
 });
+//@ End Slider Tour Detail
+
+//@ Carts
+const cart = localStorage.getItem("cart");
+//@@ Nếu chưa có giỏ hàng thì tạo giỏ hàng mới cho người dùng
+if (!cart) {
+    localStorage.setItem("cart", JSON.stringify([]));
+}
+
+//@@ Thêm tour vào cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+    formAddToCart.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const quantity = parseInt(e.target.elements.quantity.value);
+        const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+        if (quantity > 0 && tourId) {
+            const cart = JSON.parse(localStorage.getItem("cart"));
+
+            const indexExistTour = cart.findIndex(
+                (item) => item.tourId === tourId
+            );
+
+            if (indexExistTour == -1) {
+                cart.push({
+                    tourId: tourId,
+                    quantity: quantity,
+                });
+            } else {
+                cart[indexExistTour].quantity =
+                    cart[indexExistTour].quantity + quantity;
+            }
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+    });
+}
+//@ End Carts
